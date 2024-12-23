6975b835860dfd5468eb1960348f0be9c6fd4e59
vinson <vinsonleonardi@gmail.com>
2024-12-23 16:15:17 +0700

    show name in profile

--- app/src/main/java/com/example/kelompok10_tickdone/ProfileFragment.kt
+++ app/src/main/java/com/example/kelompok10_tickdone/ProfileFragment.kt
@@ -1,16 +1,32 @@
 package com.example.kelompok10_tickdone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.kelompok10_tickdone.databinding.FragmentProfileBinding
+import com.example.kelompok10_tickdone.models.User
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth;
+    private lateinit var user: FirebaseUser;
+    private lateinit var firebaseref: DatabaseReference;
+    private lateinit var userName: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +38,26 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        user = auth.currentUser!!
+        firebaseref = FirebaseDatabase.getInstance().getReference("user")
+
+        userName = binding.profileName
+        firebaseref.child(user.uid).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val userDisplay = dataSnapshot.getValue(User::class.java)
+                    if (userDisplay != null) {
+                        userName.setText(userDisplay.name)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
         // Menambahkan listener untuk mengubah nama akun
         binding.changeAccountNameLayout.setOnClickListener {
